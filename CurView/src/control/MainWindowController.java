75c417a561089a75e74a1f4acc549caf56cd723b
mojo <mojo@mojo-VirtualBox>
2020-05-27 14:18:57 -0400

    Building GUI

---
+++ CurView/src/control/MainWindowController.java
@@ -0,0 +1,19 @@
+package control;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainWindowController
+{
+	@FXML
+	private Button cmdDraw;
+	
+	//@FXML
+	//private Tab
+	
+	@FXML
+	private void initialize()
+	{
+		System.out.println("test...");
+	}
+}
