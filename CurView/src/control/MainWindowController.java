16110818df433aab1030bfdcfa2efd790200704e
mojo <mojo@mojo-VirtualBox>
2020-05-28 12:21:21 -0400

    Building GUI

--- CurView/src/control/MainWindowController.java
+++ CurView/src/control/MainWindowController.java
@@ -1,19 +1,52 @@
 package control;
 
+import java.util.HashMap;
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 
 public class MainWindowController
 {
 	@FXML
-	private Button cmdDraw;
+	private Button buttonCmdDraw;
 	
-	//@FXML
-	//private Tab
+	@FXML
+	private TabPane tabpane;
+	
+	@FXML
+	private Tab tabHilbertCurve;
+	
+	@FXML
+	private Tab tabKochCurve;
+	
+	@FXML
+	private Canvas canvasHilbertDrawTarget;
+	
+	@FXML
+	private Canvas canvasKochDrawTarget;
+	
+	@FXML
+	private Slider sliderStartX;
+	
+	@FXML
+	private Slider sliderStartY;
+	
+	@FXML
+	private Slider sliderIterations;
+	
+	@FXML
+	private Slider sliderLineWidth;
+	
+	private HashMap<Tab, Canvas> tabCanvasMap;
 	
 	@FXML
 	private void initialize()
 	{
-		System.out.println("test...");
+		tabCanvasMap = new HashMap<>();
+		tabCanvasMap.put(tabHilbertCurve, canvasHilbertDrawTarget);
+		tabCanvasMap.put(tabKochCurve, canvasKochDrawTarget);
 	}
 }
